008f110c935b7b927ece8861c000cc695409d466
Anna <annacmackenzie89@gmail.com>
2020-05-07 16:55:50 -0400

    adding styles

--- Components/Block/ProductBlock.js
+++ Components/Block/ProductBlock.js
@@ -1,20 +1,19 @@
-
 // import { documentToHtmlString } from "@contentful/rich-text-html-renderer";
-import ProductData from '../Data/ProductData'
+import ProductData from '../Data/ProductData';
 
 function ProductBlock(props) {
-    // Access post fields map
-    // console.log(props)
-    const product = props.product.fields;
-    // console.log(product)
-  
-    return (
-      <div className="card">
-          {/* <p>Block Title: {product.blockTitle}</p> */}
-          <p>Style: {product.style}</p>
-        <ProductData product={product.productComponent} />
-      </div>
-    );
-  }
-  
-  export default ProductBlock;
+  // Access post fields map
+  // console.log(props)
+  const product = props.product.fields;
+  // console.log(product)
+
+  return (
+    <div className='productSection'>
+      {/* <p>Block Title: {product.blockTitle}</p> */}
+      <p class='productHeading'>Style: {product.style}</p>
+      <ProductData product={product.productComponent} />
+    </div>
+  );
+}
+
+export default ProductBlock;
--- Components/Page/LandingPage.js
+++ Components/Page/LandingPage.js
@@ -1,31 +1,37 @@
-import ProductBlock from '../Block/ProductBlock'
-import InstructionBlock from '../Block/InstructionBlock'
-import StoryCarouselBlock from '../Block/StoryCarouselBlock'
+import ProductBlock from '../Block/ProductBlock';
+import InstructionBlock from '../Block/InstructionBlock';
+import StoryCarouselBlock from '../Block/StoryCarouselBlock';
 
 function LandingPage(props) {
-    // console.log(props.page.fields)
-    const page = props.page.fields;
-    let block_component = []
-    page.blocks.forEach(block => {
-      if (block.sys.contentType.sys.id == 'productBlock') {
-        block_component.push(<ProductBlock product={block} key={block.fields.blockTitle} />)
-      }
-      if (block.sys.contentType.sys.id == 'instructionBlock') {
-        block_component.push(<InstructionBlock block={block} key={block.fields.title} />)
-      }
-      if (block.sys.contentType.sys.id == 'storyCarouselBlock') {
-        block_component.push(<StoryCarouselBlock block={block} key={block.fields.title} />)
-      }
-    })
-    // console.log(block_component)
-  
-    return (
-      <div className="column">
-        <h1>{page.title}</h1>
-        <p>Page Description: {page.description}</p>
-          {block_component}
-      </div>
-    );
-  }
-  
-  export default LandingPage;
+  // console.log(props.page.fields)
+  const page = props.page.fields;
+  let block_component = [];
+  page.blocks.forEach(block => {
+    if (block.sys.contentType.sys.id == 'productBlock') {
+      block_component.push(
+        <ProductBlock product={block} key={block.fields.blockTitle} />
+      );
+    }
+    if (block.sys.contentType.sys.id == 'instructionBlock') {
+      block_component.push(
+        <InstructionBlock block={block} key={block.fields.title} />
+      );
+    }
+    if (block.sys.contentType.sys.id == 'storyCarouselBlock') {
+      block_component.push(
+        <StoryCarouselBlock block={block} key={block.fields.title} />
+      );
+    }
+  });
+  // console.log(block_component)
+
+  return (
+    <div className='column landing'>
+      <h1>{page.title}</h1>
+      <p class='pageDescription'>Page Description: {page.description}</p>
+      {block_component}
+    </div>
+  );
+}
+
+export default LandingPage;
