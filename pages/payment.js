c669f9838403880ab0d77bb116f94c0fc63be72f
Dylan Pomeroy <dylan@dylanpomeroy.com>
2020-05-15 15:28:59 -0400

    Replace braintree fields with hosted fields

--- pages/payment.js
+++ pages/payment.js
@@ -1,39 +1,58 @@
-import Head from "next/head";
-import { contentfulClient } from '../lib/contentful-client'
 import React, { useEffect, useState } from 'react'
-import ProductPage from '../Components/Page/ProductPage'
-import braintree from 'braintree-web-drop-in'
+import braintree_drop_in from 'braintree-web-drop-in'
+import braintree from 'braintree-web'
 import axios from 'axios'
 
 export default () => {
   
   let [submitEnabled, setSubmitEnabled] = useState(false)
-  let [dropInInstance, setDropInInstance] = useState(null)
   let [amount, setAmount] = useState('')
   let [paymentMade, setPaymentMade] = useState(false)
   let [paymentSuccessful, setPaymentSuccessful] = useState(false)
 
   useEffect(() => {
-    const configureDropin = async () => {
+    const configureHostedFields = async () => {
       const response = await axios.get('/api/braintree/clientToken')
       const clientToken = response.data.clientToken
-  
-      setDropInInstance(await braintree.create({
-        authorization: clientToken,
-        container: '#dropin-container',
-        dataCollector: {
-          kount: true,
-          paypal: true,
-        },
-        paypal: true,
-      }))
 
+      const client = await braintree.client.create({
+        authorization: clientToken,
+      })
+
+      await braintree.hostedFields.create({
+        client,
+        styles: {
+          'input': {
+            'font-size': '16pt',
+            'color': '#3A3A3A'
+          },
+          '.number': {
+            'font-family': 'monospace',
+          },
+          '.valid': {
+            'color': 'green'
+          },
+        },
+        fields: {
+          number: {
+            container: '#card-number'
+          },
+          cvv: {
+            container: '#cvv',
+            placeholder: '***',
+          },
+          expirationDate: {
+            container: '#expiration-date'
+          }
+        }
+      })
+
+      setSubmitEnabled(true)
     }
 
+    configureHostedFields()
+    
     setSubmitEnabled(true)
-
-
-    configureDropin()
   }, [])
 
   const submitPayment = () => {
@@ -65,9 +84,20 @@ export default () => {
       <p>(Hint: Test Visa number: 4111111111111111; any expiration date)</p>
       <label htmlFor="amount">Amount to pay: </label>
       <input onChange={handleAmountInputChange}></input>
-      <div id="dropin-container"></div>
-      <p>Note: Other payment options available with this dropin (if configured): Paypal, Venmo, Apple Pay, Google Pay, 3-D Secure</p>
-      <button disabled={!submitEnabled} onClick={submitPayment}>Submit Payment</button>
+
+      <form action="/" id="my-sample-form">
+        <input type="hidden" name="payment_method_nonce" />
+        <label htmlFor="card-number">Card Number: </label>
+        <div id="card-number" style={{ height: '30px', width: '250px', border: '1px solid black'}}></div>
+        <br></br>
+        <label htmlFor="cvv">CVV</label>
+        <div id="cvv" style={{ height: '30px', width: '50px', border: '1px solid black'}}></div>
+        <br></br>
+        <label htmlFor="expiration-date">Expiration Date</label>
+        <div id="expiration-date" style={{ height: '30px', width: '250px', border: '1px solid black'}}></div>
+        <br></br>
+        <input id="my-submit" type="submit" value="Pay" disabled={!submitEnabled} />
+      </form>
 
       <h3 style={{display: paymentMade && paymentSuccessful ? 'block': 'none'}}>Payment was successful! Reload page to try again</h3>
       <h3 style={{display: paymentMade && !paymentSuccessful ? 'block': 'none'}}>Payment failed. Check console or reload the page to try again</h3>
