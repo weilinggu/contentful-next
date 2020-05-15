09ed1c6c546dd74347556ffe7680667795e437b2
Sean Gillespie <info@spg.business>
2020-05-15 08:56:09 -0400

    Add scaffold for first custom resolver

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import withApollo from '../lib/with-apollo'
 import Link from 'next/link'
 import { useViewerQuery } from '../lib/viewer.graphql'
+import { useEntryQuery } from '../lib/entry.graphql'
 
 // Original Project Imports:
 
