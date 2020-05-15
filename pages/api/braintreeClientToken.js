bd15e33934b33802cc6323f41d0a8489bb954eec
Sean Gillespie <info@spg.business>
2020-05-15 02:21:21 -0400

    Add initial typescript-apollo template from Next

--- pages/api/braintreeClientToken.js
+++
@@ -1,9 +0,0 @@
-import braintree from '../../server/braintreeConnector'
-
-export default async (req, res) => {
-  const clientToken = await braintree.getClientToken('testCustomdasdfxerId')
-
-  res.json({
-    clientToken,
-  })
-}
