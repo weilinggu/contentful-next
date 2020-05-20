4a891120432f66b444cff21d29d6af87ae1d448d
Sean Gillespie <info@spg.business>
2020-05-19 23:18:23 -0400

    Simplify ContentfulClient Config

--- lib/datasources/contentfulclient.ts
+++ lib/datasources/contentfulclient.ts
@@ -1,23 +1,20 @@
 import { createClient, ContentfulClientApi } from 'contentful'
 import { SPACE, DELIVERY_TOKEN, PREVIEW_TOKEN } from '../../config.json'
-import { getOperationRootType } from 'graphql';
 
 import { DataSource } from 'apollo-datasource'
 
 const isProduction = false
 
-const isStaging = process.env.staging ? process.env.staging : false
-
 export class ContentfulClient extends DataSource {
-  client = null
+  client: ContentfulClientApi
   constructor() {
     super()
     this.client = createClient({
       host: isProduction ? "cdn.contentful.com" : "preview.contentful.com",
       // host: "cdn.contentful.com",
-      accessToken: isStaging ? process.env.DELIVERY_TOKEN : PREVIEW_TOKEN,
+      accessToken: process.env.PREVIEW_TOKEN || PREVIEW_TOKEN,
       // accessToken: DELIVERY_TOKEN,
-      space: isStaging ? process.env.SPACE : SPACE,
+      space: process.env.SPACE || SPACE,
     });
   }
   async getEntries() {
